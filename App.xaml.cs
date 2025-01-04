ce690b614e0796eb14d19136d61a6cc2610b9a6f
Bitin27 <bitin.nakarmi101@gmail.com>
2025-01-04 21:25:27 +0545

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BudgetBuddy
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
